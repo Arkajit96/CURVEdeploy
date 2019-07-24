2d0cc7d4f1316aed8fe02698c19ad8ac01643ae4
Yuening Zhu <yueningzhu505@gmail.com>
2019-07-23 21:13:55 -0400

    fourth version

--- third_back_end/routes/student.js
+++ third_back_end/routes/student.js
@@ -8,12 +8,11 @@ var Blog = require("../models/blog");
 var middlewareObj = require("../middleware");
 var mongoose = require("mongoose");
 var Institution = require("../models/institution");
-var Grid = require('gridfs-stream');
-var fs = require('fs');
 const GridFsStorage = require('multer-gridfs-storage');
 const multer = require('multer');
 const crypto = require('crypto');
 const path = require('path');
+const Grid = require('gridfs-stream');
 
 // when user login, according to the userid to get the information of this student
 router.get("/:id", middlewareObj.isLoggedIn, function(req, res, next) {
@@ -37,53 +36,80 @@ router.get("/institution/:id", middlewareObj.isLoggedIn, function(req, res, next
     })
 })
 
-router.post("/edit/resumes/:id", middlewareObj.isLoggedIn, function(req, res) {
-    var conn = mongoose.connection;
-    //connect GridFs and Mongo
-    Grid.mongo = mongoose.mongo;
-    const mongoAdd = 'mongodb+srv://yueningzhu505:volunteer123@cluster0-9ccmb.mongodb.net/curve';
-    conn.once('open', function() {
-        console.log('-Connection open--');
-        var gfs = Grid(conn.db);
-        gfs.collection('uploads');
-    })
-    student_id = req.params.id
-    console.log(req.params.id)
-    console.log(req)
-    const storage = new GridFsStorage({
-        url: mongoAdd,
-        file: (req, file) => {
-            console.log(req.body);  
-            return new Promise((resolve, reject) => {
-                crypto.randomBytes(16, (err, buf) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                gfs.files.find({"metadata.studentID": student_id}).toArray(function(err, files){
-                gfs.files.deleteOne({"metadata.studentID": student_id});
-            });
-              const filename = buf.toString('hex') + path.extname(file.originalname);
-              const studentID = student_id
-              const fileInfo = {
-                filename: filename,
-                bucketName: 'uploads',
-                metadata: {
-                  studentID: studentID
-                }
-              };
-              resolve(fileInfo, studentID);
-            });
-          });
-        }
-      });
-    const upload = multer({ storage });
-      
-    upload.single('file'),(req,res) => {
-        console.log(req.body);
-        res.json({file: req.body});
-        //console.log("upload success!")
-    };
+
+const mongoAdd = 'mongodb+srv://yueningzhu505:volunteer123@cluster0-9ccmb.mongodb.net/curve';
+const connect = mongoose.createConnection(mongoAdd);
+let gfs;
+connect.once('open',() => {
+    gfs = Grid(connect.db, mongoose.mongo);
+    gfs.collection('uploads');
 })
+const storage = new GridFsStorage({
+    url: mongoAdd,
+    file: (req, file) => {
+      console.log(req.params.id)
+      user_id = req.params.id
+      return new Promise((resolve, reject) => {
+        console.log(file.originalname);
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          gfs.files.find({"metadata.studentID": user_id}).toArray(function(err, files){
+            gfs.files.deleteOne({"metadata.studentID": user_id});
+        });
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const studentID = user_id
+          const fileInfo = {
+            filename: filename,
+            bucketName: req.params.filename,
+            metadata: {
+              studentID: user_id
+            }
+          };
+          resolve(fileInfo, studentID);
+        });
+      });
+    }
+    });
+  const upload = multer({ storage });
+  router.post("/edit/resumes/:filename/:id", upload.single('file'),(req,res) => {
+    //res.json({file: req.file});
+    console.log(1111);
+    res.json({'message':'upload successfully'});
+});
+
+
+router.get("/transcript/:filename/:id", (req, res) => {
+    gfs = Grid(connect.db, mongoose.mongo);
+    gfs.collection(req.params.filename); //set collection name to lookup into
+
+    /** First check if file exists */
+    console.log(req.params.id);
+    gfs.files.find({"metadata.studentID": req.params.id}).toArray(function(err, files){
+        if(!files || files.length === 0){
+            return res.status(404).json({
+                responseCode: 1,
+                responseMessage: "error"
+            });
+        }
+        // create read stream
+        var readstream = gfs.createReadStream({
+            filename: files[0].filename,
+            root: req.params.filename
+        });
+        // set the proper content type 
+        res.set('Content-Type', files[0].contentType)
+        //const file = `${__dirname}`;
+        //res.download(file);
+        console.log("display")
+        readstream.pipe(res);
+        //res.attachment('5.pdf');
+        //readstream1.pipe(res);
+    });   
+});
+
+
 
 router.put("/edit/:id", middlewareObj.isLoggedIn, function(req, res) {
     console.log(typeof req.body);
